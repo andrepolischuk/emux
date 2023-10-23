97f7e54dda1894f38961c7366b6803e90e9470ce
andrepolischuk <me@andrepolischuk.com>
2023-10-23 21:45:02 +0000

    Deploying to gh-pages from @ andrepolischuk/modelly@47f22ee46b1a699014d17fdb0b78519d5cd36850 🚀

---
+++ assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslJyzkjMy0vNUdJRKkgsyQAKJOckFhenFutDJfQySnJBstmZeSlKVoZGFrU6cL0+mcUlqXmpRQjNmXklqUVpiclA/TBJVAOMTM1qYwEHU91LfgAAAA=="
